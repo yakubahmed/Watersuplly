f195115c6c3187785ef77f3c64c5b720e5f2c8b7
yakubahmed <yaaqa91@gmail.com>
2020-06-30 14:03:59 +0300

    Added project files

---
+++ Water/includes/sidebar.php
@@ -0,0 +1,178 @@
+<div class="left-side-menu">
+                <div class="media user-profile mt-2 mb-2">
+                    <img src="assets/images/users/avatar-7.jpg" class="avatar-sm rounded-circle mr-2" alt="Shreyu" />
+                    <img src="assets/images/users/avatar-7.jpg" class="avatar-xs rounded-circle mr-2" alt="Shreyu" />
+
+                    <div class="media-body">
+                        <h6 class="pro-user-name mt-0 mb-0">Yakub Ahmed</h6>
+                        <span class="pro-user-desc">Administrator</span>
+                    </div>
+                    <div class="dropdown align-self-center profile-dropdown-menu">
+                        <a class="dropdown-toggle mr-0" data-toggle="dropdown" href="#" role="button" aria-haspopup="false"
+                            aria-expanded="false">
+                            <span data-feather="chevron-down"></span>
+                        </a>
+                        <div class="dropdown-menu profile-dropdown">
+                            <a href="pages-profile.html" class="dropdown-item notify-item">
+                                <i data-feather="user" class="icon-dual icon-xs mr-2"></i>
+                                <span>My Account</span>
+                            </a>
+
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i data-feather="settings" class="icon-dual icon-xs mr-2"></i>
+                                <span>Settings</span>
+                            </a>
+
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i data-feather="help-circle" class="icon-dual icon-xs mr-2"></i>
+                                <span>Support</span>
+                            </a>
+
+                            <a href="pages-lock-screen.html" class="dropdown-item notify-item">
+                                <i data-feather="lock" class="icon-dual icon-xs mr-2"></i>
+                                <span>Lock Screen</span>
+                            </a>
+
+                            <div class="dropdown-divider"></div>
+
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i data-feather="log-out" class="icon-dual icon-xs mr-2"></i>
+                                <span>Logout</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="sidebar-content">
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu" class="slimscroll-menu">
+                        <ul class="metismenu" id="menu-bar">
+                            <li class="menu-title">Navigation</li>
+
+                            <li>
+                                <a href="index.html">
+                                    <i data-feather="home"></i>
+                                    <span class="badge badge-success float-right">1</span>
+                                    <span> Dashboard </span>
+                                </a>
+                            </li>
+                            <li class="menu-title">General</li>
+           
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i data-feather="user"></i>
+                                    <span> Customer </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="email-inbox.html">Add new</a>
+                                    </li>
+                                    <li>
+                                        <a href="email-read.html">Manage</a>
+                                    </li>
+                                  
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i data-feather="home"></i>
+                                    <span> Place </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="email-inbox.html">City</a>
+                                    </li>
+                                    <li>
+                                        <a href="email-read.html">Distruct</a>
+                                    </li>
+                                    <li>
+                                        <a href="email-read.html">Zone</a>
+                                    </li>
+                                  
+                                </ul>
+                            </li>
+                           
+                            <li class="menu-title">Recieption & Charge</li>
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i data-feather="file-plus"></i>
+                                    <span> Charge </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="pages-starter.html">Add new</a>
+                                    </li>
+                                    <li>
+                                        <a href="pages-profile.html">Manage</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i data-feather="file-text"></i>
+                                    <span> Recieption </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="pages-starter.html">Add new</a>
+                                    </li>
+                                    <li>
+                                        <a href="pages-profile.html">Manage</a>
+                                    </li>
+                        
+                                </ul>
+                            </li>
+                            
+                            <li class="menu-title">Setting</li>
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i data-feather="menu"></i>
+                                    <span> Menus </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="pages-starter.html">Menu</a>
+                                    </li>
+                                    <li>
+                                        <a href="pages-profile.html">Sub menu</a>
+                                    </li>
+                        
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i data-feather="users"></i>
+                                    <span> Group & Previlage </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="pages-starter.html">Groups</a>
+                                    </li>
+                                    <li>
+                                        <a href="pages-profile.html">Previlage</a>
+                                    </li>
+                        
+                                </ul>
+                            </li>
+
+
+                        </ul>
+                    </div>
+                    <!-- End Sidebar -->
+
+                    <div class="clearfix"></div>
+                </div>
+                <!-- Sidebar -left -->
+
+            </div>
